52e4c1924b6363c8066a4db571c39c021d1b46dd
AntonZA test <anton.za@mail.ru>
2025-12-15 21:14:41 +0300

    reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-public int calculate (float height, int weight) {
-int bmi = (int) (weight / Math.pow(height, 2));
-return bmi;
-}
+    public int calculate(float height, int weight) {
+        int bmi = (int) (weight / Math.pow(height, 2));
+        return bmi;
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,10 +1,10 @@
 public class main {
     public static void main(String[] args) {
-BmiService service = new BmiService();
-float hight = 1.87f;
-int weight = 98;
-int bmi = service.calculate(hight,weight);
-System.out.println(bmi);
+        BmiService service = new BmiService();
+        float hight = 1.87f;
+        int weight = 98;
+        int bmi = service.calculate(hight, weight);
+        System.out.println(bmi);
 
     }
 }
